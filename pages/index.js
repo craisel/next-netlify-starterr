614447fa5387c202b909e804b3daa56d1323bf1d
craisel <169089892+craisel@users.noreply.github.com>
2024-05-06 16:34:30 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,41 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
-
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
-  )
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Bem-vindo ao Meu Site</title>
+<style>
+body {
+font-family: Arial, sans-serif;
+margin: 40px;
+background-color: #f4f4f9;
+color: #333;
+display: flex;
+align-items: center;
+justify-content: center;
+height: 90vh;
 }
+h1 {
+color: #026e99;
+}
+p {
+font-size: 1.2em;
+color: #666;
+}
+.container {
+text-align: center;
+padding: 20px;
+border-radius: 8px;
+background: white;
+4
+box-shadow: 0 4px 8px rgba(0,0,0,0.1);
+}
+</style>
+</head>
+<body>
+<div class="container">
+<h1>Bem-vindo ao TDE 3 - Redes</h1>
+<p>Este é um simples exemplo de DNS Gratuito Hospedado com página inicial no Netlify.</p>
+</div>
+</body>
+</html>
